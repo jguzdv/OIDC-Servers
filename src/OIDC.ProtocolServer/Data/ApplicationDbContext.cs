a27eb250cf63b13e818cadb0b10d2c6a69edf61b
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2023-08-08 17:11:44 +0200

    Move DBContext

---
+++ src/OIDC.ProtocolServer/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JGUZDV.OIDC.ProtocolServer.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    { }
+}
