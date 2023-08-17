0386e37802be74f6872d2c0544ca44c0d856b8d6
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2023-08-17 17:04:07 +0200

    Models

---
+++ src/OIDC.ProtocolServer/Data/ApplicationModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+
+using OpenIddict.Abstractions;
+
+namespace JGUZDV.OIDC.ProtocolServer.Data
+{
+    public record ApplicationModel(
+        string Id,
+        string ClientId,
+        string ConsentType,
+        string DisplayName,
+        ImmutableArray<string> Persmissions,
+        ImmutableArray<(string Type, string Value)> StaticClaims,
+        ImmutableArray<string> RequestedClaimTypes
+        )
+    {
+        public static async Task<ApplicationModel?> FromClientId(
+            IOpenIddictApplicationManager appManager,
+            string clientId, CancellationToken ct)
+        {
+            var application = await appManager.FindByClientIdAsync(clientId!, ct) ??
+            throw new InvalidOperationException("Details concerning the calling client application cannot be found.");
+
+            if (application == null)
+                return null;
+
+
+            var props = await appManager.GetPropertiesAsync(application, ct);
+            var appProps = new ApplicationProperties(props);
+
+            return new ApplicationModel(
+                await appManager.GetIdAsync(application, ct),
+                await appManager.GetClientIdAsync(application, ct),
+                await appManager.GetConsentTypeAsync(application, ct),
+                await appManager.GetDisplayNameAsync(application, ct),
+                await appManager.GetPermissionsAsync(application, ct),
+                appProps.StaticClaims.ToImmutableArray(),
+                appProps.ClaimTypes.ToImmutableArray()
+                );
+        }
+    }
+}
---
+++ src/OIDC.ProtocolServer/Data/ScopeModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Immutable;
+
+using OpenIddict.Abstractions;
+
+namespace JGUZDV.OIDC.ProtocolServer.Data
+{
+    public record ScopeModel(
+        string Name,
+        string DisplayName,
+        ImmutableArray<string> Resources,
+        ImmutableArray<(string Type, string Value)> StaticClaims,
+        ImmutableArray<string> RequestedClaimTypes
+        )
+    {
+        public static async Task<ScopeModel?> From(
+            IOpenIddictScopeManager scopeManager,
+            string scopeName, CancellationToken ct)
+        {
+            var scope = await scopeManager.FindByNameAsync(scopeName, ct);
+            if (scope == null) return null;
+
+            return await From(scopeManager, scope, ct);
+        }
+
+        private static async Task<ScopeModel?> From(IOpenIddictScopeManager scopeManager, object scope, CancellationToken ct)
+        {
+            var props = await scopeManager.GetPropertiesAsync(scope, ct);
+            var scopeProps = new ScopeProperties(props);
+
+            return new ScopeModel(
+                await scopeManager.GetNameAsync(scope, ct),
+                await scopeManager.GetDisplayNameAsync(scope, ct),
+                await scopeManager.GetResourcesAsync(scope, ct),
+                scopeProps.StaticClaims.ToImmutableArray(),
+                scopeProps.ClaimTypes.ToImmutableArray()
+                );
+        }
+    }
+}
