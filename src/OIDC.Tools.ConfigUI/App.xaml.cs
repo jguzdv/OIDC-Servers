de9df8fdf321374bcae848b8529420d1251c4dc1
Ottenhus, Thomas <Ottenhus, Thomas>
2024-05-29 13:26:42 +0200

    Add project stub for config tool

---
+++ src/OIDC.Tools.ConfigUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace JGUZDV.OIDC.Tools.ConfigUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ src/OIDC.Tools.ConfigUI/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace JGUZDV.OIDC.Tools.ConfigUI;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
