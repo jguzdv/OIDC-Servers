97c69c46d3cbae87803e8bc37e20693e8d540708
Ottenhus, Thomas <Ottenhus, Thomas>
2024-11-07 10:49:02 +0100

    Allow forcing array semantics to a claim type

--- src/OIDC.ProtocolServer/OpenIddictExt/ClaimsIdentityExtensions.cs
+++ src/OIDC.ProtocolServer/OpenIddictExt/ClaimsIdentityExtensions.cs
@@ -7,18 +7,23 @@ namespace JGUZDV.OIDC.ProtocolServer.OpenIddictExt
 {
     public static class ClaimsIdentityExtensions
     {
+        // Some claims are defined to be arrays by the OIDC spec. We'll need to handle them accordingly.
+        private static readonly HashSet<string> _arrayTypeClaims = ["amr"];
+
         public static void SetClaims(this ClaimsIdentity identity, IEnumerable<Model.Claim> claims)
         {
             // Claims may be single value or multi value. So we group by type and add them accordingly.
             foreach (var claimTypeClaims in claims.GroupBy(x => x.Type, StringComparer.OrdinalIgnoreCase))
             {
-                if (claimTypeClaims.Count() == 1)
+                var forceArraySemantics = _arrayTypeClaims.Contains(claimTypeClaims.Key);
+
+                if(forceArraySemantics || claimTypeClaims.Count() > 1)
                 {
-                    identity.SetClaim(claimTypeClaims.Key, claimTypeClaims.First().Value);
+                    identity.SetClaims(claimTypeClaims.Key, claimTypeClaims.Select(x => x.Value).Distinct().ToImmutableArray());
                 }
                 else
                 {
-                    identity.SetClaims(claimTypeClaims.Key, claimTypeClaims.Select(x => x.Value).Distinct().ToImmutableArray());
+                    identity.SetClaim(claimTypeClaims.Key, claimTypeClaims.First().Value);
                 }
             }
         }
