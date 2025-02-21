fd915aa654198989912af7293ad09d142232688e
Ottenhus, Thomas <Ottenhus, Thomas>
2025-02-21 09:50:11 +0100

    Using a record for value based comparability

--- src/OIDC.ProtocolServer.Model/Claim.cs
+++ src/OIDC.ProtocolServer.Model/Claim.cs
@@ -1,15 +1,14 @@
 ﻿
-namespace JGUZDV.OIDC.ProtocolServer.Model
-{
-    public class Claim // TODO: this should probably be a record
-    {
-        public Claim(string type, string value)
-        {
-            Type = type ?? throw new ArgumentNullException(nameof(type));
-            Value = value ?? throw new ArgumentNullException(nameof(value));
-        }
+namespace JGUZDV.OIDC.ProtocolServer.Model;
 
-        public string Type { get; }
-        public string Value { get; }
+public record Claim
+{
+    public Claim(string type, string value)
+    {
+        Type = type ?? throw new ArgumentNullException(nameof(type));
+        Value = value ?? throw new ArgumentNullException(nameof(value));
     }
+
+    public string Type { get; init; }
+    public string Value { get; init; }
 }
