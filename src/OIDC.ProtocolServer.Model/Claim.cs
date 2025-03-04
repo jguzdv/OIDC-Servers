67fe4bd7b4175d6f6b32607f39c0f52b555031a0
Ottenhus, Thomas <Ottenhus, Thomas>
2025-03-04 17:33:39 +0100

    Add missing json converter

--- src/OIDC.ProtocolServer.Model/Claim.cs
+++ src/OIDC.ProtocolServer.Model/Claim.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace JGUZDV.OIDC.ProtocolServer.Model;
@@ -11,3 +12,59 @@ public record Claim(ClaimType Type, ClaimValue Value)
     { }
 }
 
+public class ClaimJsonConverter : JsonConverter<Claim>
+{
+    public override Claim Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string? type = null;
+        string? value = null;
+
+        for (int i = 0; i < 2; i++)
+        {
+            if (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.PropertyName)
+                {
+                    var propertyName = reader.GetString() ?? "-";
+
+                    if(propertyName.Equals("Type", StringComparison.OrdinalIgnoreCase))
+                    {
+                        reader.Read();
+                        type = reader.GetString();
+                    }
+                    else if (propertyName.Equals("Value", StringComparison.OrdinalIgnoreCase))
+                    {
+                        reader.Read();
+                        value = reader.GetString();
+                    }
+                    else
+                    {
+                        throw new JsonException();
+                    }
+                }
+                else
+                {
+                    throw new JsonException();
+                }
+            }
+            else
+            {
+                throw new JsonException();
+            }
+        }
+
+        if(type is null || value is null)
+        {
+            throw new JsonException();
+        }
+
+        return new Claim(type, value);
+    }
+    public override void Write(Utf8JsonWriter writer, Claim value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        writer.WriteString("Type", value.Type.Type);
+        writer.WriteString("Value", value.Value.Value);
+        writer.WriteEndObject();
+    }
+}
