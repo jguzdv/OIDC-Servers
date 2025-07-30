a3592ab542522004efa5f906cf76f597c4ed1d27
Ottenhus, Thomas <Ottenhus, Thomas>
2025-07-30 14:39:02 +0200

    Updates

--- src/OIDC.OpenIdDict.ConfigUI/Program.cs
+++ src/OIDC.OpenIdDict.ConfigUI/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using JGUZDV.OIDC.ConfigUI.Data;
+﻿using JGUZDV.OIDC.ConfigUI.Data;
 using JGUZDV.OIDC.ProtocolServer.Model;
 
 using Microsoft.Data.SqlClient;
